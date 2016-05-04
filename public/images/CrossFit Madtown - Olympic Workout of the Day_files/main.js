9c4ab675d9821734e3b897e04dfe3a463704e782
djdurban <shawn@mccrays.net>
2016-05-04 11:32:01 -0500

    dynamodb !!

---
+++ public/images/CrossFit Madtown - Olympic Workout of the Day_files/main.js
@@ -0,0 +1,55 @@
+var pathname = window.location.pathname;
+
+$('#thumbnails ul li a').click(function(e){
+    e.preventDefault();
+    var linkId = $(this).attr('data-ui');
+    var modalId = $('#' + linkId);
+    if(linkId === modalId.attr('id')){
+        $('.modal-wrapper').toggleClass('modal-open');
+        $('.modal-open').css('height', $( window ).height());
+    }
+
+    $('.modal-open').click(function(){
+        $(this).removeClass('modal-open');
+    });
+});
+
+var interval = undefined;
+$(document).ready(function () {
+    var current = moment();
+    var start = moment("4/23/2016 8:30 am");
+    var end = moment("4/24/2016 6:00 pm");
+    var btnLiveStream = $('#btnLiveStream');
+    var lblLiveStream = $('#lblLiveStream');
+    var btnMenu = $('#menu-btn');
+    var header = $('#header');
+    var btnMenuClose = $('#menu-close');
+
+    window.carousel('.carousel');
+
+    btnLiveStream.on('click', function (ev) {
+        if (current < start) {
+            alert('Live Stream has not started yet, please check back Saturday after 8:30am');
+            ev.stopPropagation();
+            ev.preventDefault();
+        }
+    });
+
+    btnMenu.on('click', function (ev) {
+        ev.preventDefault();
+        header.toggleClass('show-nav');
+    });
+
+    btnMenuClose.on('click', function (ev) {
+        ev.preventDefault();
+        header.removeClass('show-nav');
+    });
+
+    if (current > end) {
+        btnLiveStream.hide();
+        lblLiveStream.hide();
+    }
+});
+
+
+
