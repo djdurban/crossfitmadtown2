1715a941955e6921671af0bc6e89611f6a8e584e
Shawn McCray <shawn@mccrays.net>
2016-05-04 17:02:24 -0500

    added new relic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['CrossFit Madtown'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'd128d1a8ee6e8704b395783c9b758c4d9b3564f6',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
